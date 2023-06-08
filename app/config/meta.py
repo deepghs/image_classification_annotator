be7b7d865217ba044d20a1865665bd95d4f4785a
narugo1992 <narugo@126.com>
2023-06-08 17:07:38 +0800

    dev(narugo): fix actions

--- app/config/meta.py
+++ app/config/meta.py
@@ -4,16 +4,16 @@ Overview:
 """
 
 #: Title of this project (should be `app`).
-__TITLE__ = "pyqt5-demo"
+__TITLE__ = "image_classification_annotator"
 
 #: Version of this project.
-__VERSION__ = "0.0.3"
+__VERSION__ = "0.0.1"
 
 #: Short description of the project, will be included in ``setup.py``.
-__DESCRIPTION__ = 'Demo Application of PyQt5.'
+__DESCRIPTION__ = 'Annotator for classifying images.'
 
 #: Author of this project.
-__AUTHOR__ = "HansBug"
+__AUTHOR__ = "narugo1992"
 
 #: Email of the authors'.
-__AUTHOR_EMAIL__ = "hansbug@buaa.edu.cn"
+__AUTHOR_EMAIL__ = "narugo@126.com"
