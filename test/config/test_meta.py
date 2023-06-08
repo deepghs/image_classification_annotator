76019a51c8c1ee950b5bdccc3cbc63fac6259ef2
narugo1992 <narugo@126.com>
2023-06-08 17:10:45 +0800

    dev(narugo): fix actions

--- test/config/test_meta.py
+++ test/config/test_meta.py
@@ -6,4 +6,4 @@ from app.config.meta import __TITLE__
 @pytest.mark.unittest
 class TestConfigMeta:
     def test_title(self):
-        assert __TITLE__ == 'pyqt5-demo'
+        assert __TITLE__ == 'image_classification_annotator'
